e6d40d7be9b387b2dad13807162636d14ed30683
arthur.tavaresquintao <arthur.tavaresquintao@student.kuleuven.be>
2023-04-17 21:09:29 +0200

    comment out code not used for test

--- main/main.c
+++ main/main.c
@@ -1,24 +1,24 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <stdio.h>
-#include "audio.h"
-#include "transport.h"
+// #include "audio.h"
+// #include "transport.h"
 
-static StreamBufferHandle_t mic_stream_buf;
-static StreamBufferHandle_t network_stream_buf;
+// static StreamBufferHandle_t mic_stream_buf;
+// static StreamBufferHandle_t network_stream_buf;
+
+static const char* TAG = "main";
 
 void app_main(void) {
-    mic_stream_buf = xStreamBufferCreate(512, 1);
-    network_stream_buf = xStreamBufferCreate(512, 1);
 
-    init_transport(mic_stream_buf, network_stream_buf);
-    init_audio(mic_stream_buf, network_stream_buf);
+    ESP_LOGI(TAG, "Starting program");
+
+    // mic_stream_buf = xStreamBufferCreate(512, 1);
+    // network_stream_buf = xStreamBufferCreate(512, 1);
+
+    // init_transport(mic_stream_buf, network_stream_buf);
+    // init_audio(mic_stream_buf, network_stream_buf);
 
     // Loopback testing
     // init_audio(mic_stream_buf, mic_stream_buf);
-
-    while (true) {
-        printf("Hello world!\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
 }
