4eb259248705483357500bf67d6c28c8f9f1dde8
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-04-27 19:57:48 +0200

    move definitions

--- main/config.h
+++ main/config.h
@@ -3,3 +3,9 @@
 #include "freertos/task.h"
 #include "freertos/stream_buffer.h"
 #include "esp_log.h"
+
+#define WIFI_CHANNEL (12)
+#define STATE_CHANNEL (0)
+
+#define ESPNOW_TX_MODE (1)
+#define ESPNOW_RX_MODE (0)
