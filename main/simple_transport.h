06515071f663782fbe8cf380a41eb6c2dac9f2ea
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-04-27 18:39:05 +0200

    add signal RX and TX functions to .h

--- main/simple_transport.h
+++ main/simple_transport.h
@@ -1,8 +1,21 @@
 #ifndef TRANSPORT_H
 #define TRANSPORT_H
+#endif
 
 #include "config.h"
 
+typedef enum {
+    RX_STATE,
+    TX_STATE
+
+} fsm_state_t;
+
 void init_simple_transport(void);
 
-#endif
+static fsm_state_t peer_state;
+
+void signal_RX(void);
+
+void signal_TX(void);
+
+void sender_task(void* arg);
