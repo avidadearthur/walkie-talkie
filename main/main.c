eaa8b7a2ad28fdc0cf15352b5b59fe065f7f077c
arthur <arthur.tavaresquintao@student.kuleuven.be>
2023-05-03 12:51:52 +0200

    multiple definition of `my_state';

--- main/main.c
+++ main/main.c
@@ -48,12 +48,8 @@ void init_gipio(void) {
 }
 /*--------------------------------------------------------------------------------------*/
 
-#if (RECV)
 static StreamBufferHandle_t network_stream_buf; // only for reciever
-#else
 static StreamBufferHandle_t mic_stream_buf;
-static StreamBufferHandle_t record_stream_buf; // only for transmitter
-#endif
 
 void init(void) {
     // deafult transmission rate of esp_now_send is 1Mbps = 125KBps, stream buffer size has to be
